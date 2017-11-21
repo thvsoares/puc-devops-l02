16cfe841eaa472187bf5749614d3e5ed5c18cdac
Thiago Soares <thiagomullet@gmail.com>
2017-11-21 20:58:10 -0200

    Project structure

---
+++ Lista2DevOps/Lista2DevOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista2DevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lista2DevOps/Lista2DevOpsTest/RomanConverterUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lista2DevOpsTest
+{
+    [TestClass]
+    public class RomanConverterUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
